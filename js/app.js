e6383e31d60016c7d6c7727a1176344ec96a0b92
Amit V Navale <amit.navale@gmail.com>
2015-08-29 21:15:39 +1000

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+﻿
+var dragdropModule = angular.module('dragdrop', ['ui']);
+
+dragdropModule.controller('dragdropController', [
+	'$scope',
+	function ($scope) {
+	    $scope.msg = "hi";
+	    $scope.list = [
+            { name: "beer", imageUrl: "/images/beer.jpg" },
+            { name: "duck", imageUrl: "/images/duck.jpg" },
+            { name: "lion", imageUrl: "/images/lion.jpg" },
+            { name: "puppy", imageUrl: "/images/puppy.jpg" },
+            { name: "squirel", imageUrl: "/images/squirel.jpg" },
+	    ];
+	}]);
+
